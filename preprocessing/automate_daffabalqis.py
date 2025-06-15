dbdc53855ba206bd8f3ccba941aaada5ad1c5f5e
Daffa Akifah Balqis <92258951+daffaakifah@users.noreply.github.com>
2025-06-15 14:26:00 +0700

    Update automate_daffabalqis.py

--- preprocessing/automate_daffabalqis.py
+++ preprocessing/automate_daffabalqis.py
@@ -7,6 +7,7 @@ Fungsi utama:
 
 import pandas as pd
 from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import StandardScaler
 
